298f1a27b7015726ecea663f9862246444ec0ed2
tomoya takamori <tomoya.takamori19@gmail.com>
2020-10-03 09:56:05 +0900

    write config.fog_credentials

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -3,10 +3,10 @@ if Rails.env.production?
     config.fog_credentials = {
       # Amazon S3用の設定
       :provider              => 'AWS',
-      :region                => ENV['S3_REGION'],     # 例: 'ap-northeast-1'
-      :aws_access_key_id     => ENV['S3_ACCESS_KEY'],
-      :aws_secret_access_key => ENV['S3_SECRET_KEY']
+      :region                => 'ap-northeast-1',     # 例: 'ap-northeast-1'
+      :aws_access_key_id     => 'AKIAS4UR4KG4A3GZQA5R',
+      :aws_secret_access_key => 'SHglidQRq7syu77mhaZt+ErZdV6GXnOZL95i1cq3'
     }
-    config.fog_directory     =  ENV['S3_BUCKET']
+    config.fog_directory     =  'sample.app.takamori'
   end
 end
