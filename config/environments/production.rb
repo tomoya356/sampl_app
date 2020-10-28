65bed57b56416b6e57376cb2f3bf5f5581a9066e
tomoya takamori <tomoya.takamori19@gmail.com>
2020-10-28 16:25:21 +0900

    change production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,7 +66,7 @@ Rails.application.configure do
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = 'https://tomm19-sample-app.herokuapp.com'
+  host = 'https://boiling-spire-54043.herokuapp.com'
   config.action_mailer.default_url_options = { host: host, protocol: 'https' }
   ActionMailer::Base.smtp_settings = {
     :address        => 'smtp.sendgrid.net',
